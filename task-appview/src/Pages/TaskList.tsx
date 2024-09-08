f9c53dd389f7c2a93bad25c15a0c95d44ad84192
tarotaros <y.t.tanpopo.15221@gmail.com>
2024-09-08 21:05:57 +0900

    [add]レコード削除確認画面

--- task-appview/src/Pages/TaskList.tsx
+++ task-appview/src/Pages/TaskList.tsx
@@ -1,94 +1,108 @@
-import { Modal } from '@mui/material';
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import { IconButton, Modal } from '@mui/material';
+import Grid from '@mui/material/Grid2';
 import Paper from '@mui/material/Paper';
-import { DataGrid, GridColDef, GridRowParams } from '@mui/x-data-grid';
+import { DataGrid, GridActionsCellItem, GridColDef } from '@mui/x-data-grid';
 import { useState } from 'react';
+import RemoveConfirmModal from "./RemoveConfirmModal";
 import TaskEditModal from './TaskEditModal';
 
-const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
-    {
-        field: 'age',
-        headerName: 'Age',
-        type: 'number',
-        width: 90,
-    },
-    {
-        field: 'fullName',
-        headerName: 'Full name',
-        description: 'This column has a value getter and is not sortable.',
-        sortable: false,
-        width: 160,
-        valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
-    },
-];
 
 const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 1, title: 'title', priority: 0, status: 0, deadline: '2024/01/01', start: '2024/01/01', finish: '2024/01/01', manager: '0' },
+    { id: 2, title: 'title', priority: 1, status: 1, deadline: '2024/01/01', start: '2024/01/01', finish: '2024/01/01', manager: '0' },
+    { id: 3, title: 'title', priority: 2, status: 2, deadline: '2024/01/01', start: '2024/01/01', finish: '2024/01/01', manager: '0' },
+    { id: 4, title: 'title', priority: 0, status: 0, deadline: '2024/01/01', start: '2024/01/01', finish: '2024/01/01', manager: '0' },
+    { id: 5, title: 'title', priority: 1, status: 1, deadline: '2024/01/01', start: '2024/01/01', finish: '2024/01/01', manager: '0' },
+    { id: 6, title: 'title', priority: 2, status: 2, deadline: '2024/01/01', start: '2024/01/01', finish: '2024/01/01', manager: '0' },
+    { id: 7, title: 'title', priority: 0, status: 0, deadline: '2024/01/01', start: '2024/01/01', finish: '2024/01/01', manager: '0' },
+    { id: 8, title: 'title', priority: 1, status: 1, deadline: '2024/01/01', start: '2024/01/01', finish: '2024/01/01', manager: '0' },
+    { id: 9, title: 'title', priority: 2, status: 2, deadline: '2024/01/01', start: '2024/01/01', finish: '2024/01/01', manager: '0' },
 ];
 
-
-const customStyles = {
-    content: {
-        top: "20%",
-        left: "50%",
-        right: "auto",
-        bottom: "auto",
-        marginRight: "-50%",
-        transform: "translate(-50%, -50%)",
-        minWidth: "40%",
-    },
-};
-
-
-const modalStyle: React.CSSProperties = {
-    backgroundColor: '#fff',
-    padding: '100px 400px',
-    borderRadius: '10px',
-};
-
-
-const paginationModel = { page: 0, pageSize: 5 };
+const paginationModel = { page: 0, pageSize: 20 };
 
 export default function DataTable() {
 
-    const [isOpen, setIsOpen] = useState(false);
-    const handleOpenModal = () => setIsOpen(true);
-    const handleCloseModal = () => setIsOpen(false);
+    const columns: GridColDef[] = [
+        { field: 'id', headerName: 'ID', width: 70 },
+        { field: 'title', headerName: 'タイトル', width: 130 },
+        { field: 'priority', headerName: '優先度', width: 130 },
+        { field: 'status', headerName: 'ステータス', width: 130 },
+        { field: 'deadline', headerName: '期限', width: 130 },
+        { field: 'start', headerName: '開始日', width: 130 },
+        { field: 'finish', headerName: '終了日', width: 130 },
+        { field: 'manager', headerName: '担当者', width: 130 },
+        {
+            field: 'actions',
+            type: 'actions',
+            headerName: '編集',
+            width: 100,
+            cellClassName: 'actions',
+            getActions: () => {
+                return [
+                    <GridActionsCellItem
+                        icon={<EditIcon />}
+                        label="Edit"
+                        className="textPrimary"
+                        onClick={handleOpenEditModal}
+                        color="inherit"
+                    />
+                ];
+            },
+        },
+    ];
 
+    const [isEditModalOpen, setEditModalIsOpen] = useState(false);
+    const handleOpenEditModal = () => setEditModalIsOpen(true);
+    const handleCloseEditModal = () => setEditModalIsOpen(false);
 
-    const handleRowClick = (params: GridRowParams) => {
-        handleOpenModal();
-    };
+    const [isDeleteConfirmModalOpen, setRemoveConfirmModalIsOpen] = useState(false);
+    const handleOpenDeleteConfirmModal = () => setRemoveConfirmModalIsOpen(true);
+    const handleCloseDeleteConfirmModal = () => setRemoveConfirmModalIsOpen(false);
 
+    const handleDeleteModal = () => {
+
+    }
 
     return (
-        <Paper sx={{ height: 400, width: '100%' }}>
-            <div>
-                <Modal open={isOpen} onClose={handleCloseModal}>
-                    <TaskEditModal handleCloseModal={handleCloseModal} />
+        <div>
+            <Grid container spacing={2} justifyContent="center" alignItems="center">
+                <Grid size={12}>
+                    <div>
+                        <Modal open={isEditModalOpen}>
+                            <TaskEditModal handleCloseModal={handleCloseEditModal} />
+                        </Modal>
+                        <Modal open={isDeleteConfirmModalOpen}>
+                            <RemoveConfirmModal
+                                handleCancelModal={handleCloseDeleteConfirmModal}
+                                handleDeleteModal={handleDeleteModal} />
+                        </Modal>
+                    </div>
+                </Grid>
+                <Grid my={2} size={12} justifyContent="end" spacing={1} container>
+                    <Grid >
+                        <IconButton aria-label="add" onClick={handleOpenEditModal}><AddIcon /></IconButton>
+                    </Grid>
+                    <Grid >
+                        <IconButton aria-label="delete" onClick={handleOpenDeleteConfirmModal} ><DeleteIcon /></IconButton>
+                    </Grid>
+                </Grid>
 
-                </Modal>
-            </div>
-            <DataGrid
-                onRowClick={handleRowClick}
-                //onRowClick={open}
-                rows={rows}
-                columns={columns}
-                initialState={{ pagination: { paginationModel } }}
-                pageSizeOptions={[5, 10]}
-                //checkboxSelection
-                sx={{ border: 0 }}
-            />
-        </Paper>
+                <Grid size={12}>
+                    <Paper sx={{ height: '90%', width: 'auto' }}>
+                        <DataGrid
+                            rows={rows}
+                            checkboxSelection
+                            columns={columns}
+                            initialState={{ pagination: { paginationModel } }}
+                            pageSizeOptions={[5, 10]}
+                            sx={{ border: 0 }}
+                        />
+                    </Paper>
+                </Grid></Grid>
+        </div>
     );
 }
