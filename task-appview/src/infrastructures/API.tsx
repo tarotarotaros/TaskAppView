93c0e56737815e863a801d3a27acffe0df4c6a08
tarotaros <y.t.tanpopo.15221@gmail.com>
2024-10-14 19:20:42 +0900

    [fix]api-url

--- task-appview/src/infrastructures/API.tsx
+++ task-appview/src/infrastructures/API.tsx
@@ -1,3 +1,4 @@
-export const BASE_URL = 'http://localhost:8000/api/'
+export const API_URL = process.env.API_URL
+export const BASE_URL = `${API_URL}/api/`
 export const getAuthToken = () => sessionStorage.getItem('authToken');
 
