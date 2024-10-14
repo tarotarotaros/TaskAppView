e3971228891c2b6cabecb0ee2258d90665d655ca
tarotaros <y.t.tanpopo.15221@gmail.com>
2024-10-14 10:06:41 +0900

    [fix]buildtool:cra->vite

---
+++ task-appview/vite.config.ts
@@ -0,0 +1,16 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    server: {
+        host: true,
+        open: true,
+    },
+    resolve: {
+        alias: [{ find: "@", replacement: "/src" }],
+    },
+    build: {
+        outDir: "build",
+    },
+    plugins: [react()],
+});
