49bdda4e9b6b8eeb5e787fee7baf30380cff00dc
tarotaros <y.t.tanpopo.15221@gmail.com>
2024-10-14 21:10:45 +0900

    [test]API_URLの変数名4

--- task-appview/src/App.tsx
+++ task-appview/src/App.tsx
@@ -2,8 +2,10 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './features/home/components/Home';
+import { API_URL } from './infrastructures/API';
 
 function App() {
+  console.log("API:" + API_URL);
   return (
     <Routes>
       <Route path="/" element={<Home />} />
