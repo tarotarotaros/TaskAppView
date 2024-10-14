c03aeb68529ffa59b9a14e9cb22ac6bdd932f139
tarotaros <y.t.tanpopo.15221@gmail.com>
2024-10-14 20:53:39 +0900

    [test]API_URLの変数名

--- task-appview/src/infrastructures/API.tsx
+++ task-appview/src/infrastructures/API.tsx
@@ -1,4 +1,4 @@
-export const API_URL = process.env.API_URL
+export const API_URL = process.env.NEXT_PUBLIC_API_URL
 export const BASE_URL = `${API_URL}/api/`
 export const getAuthToken = () => sessionStorage.getItem('authToken');
 
