2d1033738035a114311c38a87fc083d443f4261d
tarotaros <y.t.tanpopo.15221@gmail.com>
2024-10-16 22:44:00 +0900

    [fix]test

--- task-appview/src/tests/Home.test.tsx
+++
@@ -1,9 +0,0 @@
-import '@testing-library/jest-dom/extend-expect';
-import { render, screen } from '@testing-library/react';
-import Home from '../features/home/components/Home';
-
-test('renders hello message', () => {
-    render(<Home />);
-    const helloElement = screen.getByText(/Home/i);
-    expect(helloElement).toBeInTheDocument();
-});
